a40755f2a561676b8a7b38bfebf07707a2c44a75
Roycewrites89 <roycedaniel909@gmail.com>
2026-01-30 15:51:04 -0600

    Add files via upload

---
+++ myprogram_1.py
@@ -0,0 +1,6 @@
+#Royce Daniel. 1/30/26 "Stein's personal data"
+name = "Stein"
+address = "6742 Carterville lane. Burgberg, NY, 10001"
+phone = "767 420 1234"
+major = "Art History"
+print (name + address + phone + major)
